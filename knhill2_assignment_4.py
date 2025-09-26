b555a19a0bb12d2677b8dd18f528df3cb4c770f5
Kabijah <kabijahhill2000@gmail.com>
2025-09-26 10:51:23 -0400

    Update knhill2_assignment_4.py

--- knhill2_assignment_4.py
+++ knhill2_assignment_4.py
@@ -7,3 +7,34 @@ stress_level = 20
 print("🚂 Welcome to the Success Express Station!")
 print(f"Student: {student_name}")
 print(f"Starting GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress: {stress_level}")
+#blah blah blah step 2
+print("\nChoose your course load:")
+print("A) Light Load (12 credits)")
+print("B) Standard Load (15 credits)")
+print("C) Heavy Load (18 credits)")
+
+choice = input("Your choice: ")
+if choice == "A":
+    stress_level -= 5
+    social_points += 10
+    print("Hooray for no stress, success!")
+elif choice == "B":
+    study_hours += 10
+    stress_level += 5
+    print("Good choice! Balance goes a long way.")
+elif choice == "C":
+    if current_gpa >= 3.5:
+        study_hours += 20
+        stress_level += 15
+        print("This is more than the average, but nothing's impossible. You got this!  ")
+else:
+    print("Invalid choice!")
+
+
+
+
+
+
+
+
+
