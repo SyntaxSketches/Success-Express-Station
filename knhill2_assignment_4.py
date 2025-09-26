66b54c0b93825d059d92aaeed0cf49038038f625
Kabijah <kabijahhill2000@gmail.com>
2025-09-26 10:27:48 -0400

    Update knhill2_assignment_4.py
    step one

--- knhill2_assignment_4.py
+++ knhill2_assignment_4.py
@@ -1,1 +1,9 @@
+student_name = "YourName"
+current_gpa = 3.2
+study_hours = 23
+social_points = 15
+stress_level = 20
 
+print("🚂 Welcome to the Success Express Station!")
+print(f"Student: {student_name}")
+print(f"Starting GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress: {stress_level}")
